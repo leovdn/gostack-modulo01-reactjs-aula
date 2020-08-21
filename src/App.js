42822e42e7b63a1666542ec0f87a66a32e663218
leovdn <leo_vdn@hotmail.com>
2020-08-20 22:40:57 -0300

    useState for new project list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Fron-end Web']);
+
+  function handleAddProject() {
+    // projects.push(`Novo Projeto ${Date.now()}`);
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
+
+
   return (
   <>
-    <Header title="Homepage">
-      <ul>
-        <li>Sobre</li>
-        <li>Contato</li>
-        <li>Links</li>
-        <li>Github</li>
-      </ul>
-    </Header>
-    <Header title="Projects">
-      <ul>
-        <li>Galeria</li>
-        <li>Formulário</li>
-      </ul>
-    </Header>
+    <Header title="Projects" />
+
+    <ul>
+      {projects.map(project => <li key={project}>{project}</li>)}
+    </ul>
+
+    <button type="button" onClick={handleAddProject}>Add Project</button>
   </>
   )
 }
