e38f16c52b7e460ca6e92560dfb5ba8da81bfda4
leovdn <leo_vdn@hotmail.com>
2020-08-21 03:45:00 -0300

    conexão com backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import Header from './components/Header';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Fron-end Web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);      
+    });
+  }, [])
 
   function handleAddProject() {
     // projects.push(`Novo Projeto ${Date.now()}`);
     setProjects([...projects, `Novo Projeto ${Date.now()}`]);
-
-    console.log(projects);
   }
 
 
@@ -20,10 +24,9 @@ function App() {
   <>
     <Header title="Projects" />
 
-    <img width={300} src={backgroundImage} alt=""/>
 
     <ul>
-      {projects.map(project => <li key={project}>{project}</li>)}
+      {projects.map(project => <li key={project.id}>{project.title}</li>)}
     </ul>
 
     <button type="button" onClick={handleAddProject}>Add Project</button>
